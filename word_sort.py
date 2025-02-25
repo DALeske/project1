515cd6929909f8cbe0c8c43c96d243fc2135cbc0
David Leske <leske.david@gmail.com>
2025-02-24 18:23:19 -0600

    update word_sort - lower

--- word_sort.py
+++ word_sort.py
@@ -1,4 +1,4 @@
 # code to sort the words in a sentence in alphabetical order
-string_in = input("Enter a sentence:")
+string_in = input("Enter a sentence:").lower()
 words = sorted(string_in.split())
 print("Here are the sorted words in your text:""\n","\n".join(words))
